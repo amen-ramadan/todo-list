32398b968ab2911115929f96db15dd8b819a4a42
amen <ramadanamen407@gmail.com>
2024-03-21 17:47:23 +0300

    now time a new branch

--- src/Components/Todo/Todo.jsx
+++ src/Components/Todo/Todo.jsx
@@ -1,42 +1,17 @@
-import {
-  Card,
-  CardContent,
-  Grid,
-  IconButton,
-  Typography,
-  Button,
-  TextField,
-} from "@mui/material";
+import { Card, CardContent, Grid, IconButton, Typography } from "@mui/material";
 import DoneIcon from "@mui/icons-material/Done";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 
-// import dialogs
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
-
 // context
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import TodosContext from "../../context/TodosContext";
 
-export default function Todo({ todo }) {
+export default function Todo({ todo, showDelete, showUpdate }) {
   // import context
   const { todos, setTodos } = useContext(TodosContext);
 
-  // state for dialog state change
-  const [openDialog, setOpenDialog] = useState(false);
-  const [showUpdateDialog, setShowUpdateDialog] = useState(false);
-  const [updateTodo, setUpdateTodo] = useState({
-    title: todo.title,
-    details: todo.details,
-  });
-
   // event handlers 💀💀💀💀
-
-  // pass through the todo id for parent component
   function handleCheckClick() {
     const newUpdateTodos = todos.map((t) => {
       if (t.id === todo.id) {
@@ -48,40 +23,6 @@ export default function Todo({ todo }) {
     localStorage.setItem("todos", JSON.stringify(newUpdateTodos));
   }
 
-  // delete function and close the dialog
-  function handleDeleteClick() {
-    setOpenDialog(true);
-  }
-
-  function handleDeleteDialogClose() {
-    setOpenDialog(false);
-  }
-
-  function handleDeleteConfirm() {
-    const newUpdateTodos = todos.filter((t) => t.id !== todo.id);
-    setTodos(newUpdateTodos);
-    // handleClose(false);
-    localStorage.setItem("todos", JSON.stringify(newUpdateTodos));
-  }
-
-  function handleUpdateClick() {
-    setShowUpdateDialog(true);
-  }
-
-  function handleUpdateClose() {
-    setShowUpdateDialog(false);
-  }
-
-  function handleUpdateConfirm() {
-    const newUpdateTodos = todos.map((t) => {
-      return todo.id === t.id
-        ? { ...t, title: updateTodo.title, details: updateTodo.details }
-        : t;
-    });
-    setTodos(newUpdateTodos);
-    setShowUpdateDialog(false);
-    localStorage.setItem("todos", JSON.stringify(newUpdateTodos));
-  }
   // ==== event handlers ==== 💀💀💀💀
 
   const iconStyles = {
@@ -94,78 +35,6 @@ export default function Todo({ todo }) {
 
   return (
     <>
-      {/* DELETE DIALOG */}
-      <Dialog
-        open={openDialog}
-        onClose={handleDeleteDialogClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          {"Are you sure you want to delete this task?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            You cannot undo the deletion after it is complete.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleDeleteDialogClose}>Disagree</Button>
-          <Button onClick={handleDeleteConfirm} autoFocus>
-            Delete
-          </Button>
-        </DialogActions>
-      </Dialog>
-      {/*=== DELETE DIALOG ===*/}
-      {/* UPDATE DIALOG */}
-      <Dialog
-        open={showUpdateDialog}
-        onClose={handleUpdateClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          {"Edit the assignment "}
-        </DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            required
-            margin="dense"
-            id="name"
-            name="email"
-            label="title task name"
-            fullWidth
-            variant="standard"
-            value={updateTodo.title}
-            onChange={(e) => {
-              setUpdateTodo({ ...updateTodo, title: e.target.value });
-            }}
-          />
-          <TextField
-            autoFocus
-            required
-            margin="dense"
-            id="name"
-            name="email"
-            label="details"
-            fullWidth
-            variant="standard"
-            value={updateTodo.details}
-            onChange={(e) => {
-              setUpdateTodo({ ...updateTodo, details: e.target.value });
-            }}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleUpdateClose}>Disagree</Button>
-          <Button onClick={handleUpdateConfirm} autoFocus>
-            Agree
-          </Button>
-        </DialogActions>
-      </Dialog>
-      {/*=== UPDATE DIALOG ===*/}
-
       <Card
         sx={{
           backgroundColor: "#0097a7",
@@ -207,7 +76,9 @@ export default function Todo({ todo }) {
 
               {/* UPDATE BUTTON */}
               <IconButton
-                onClick={handleUpdateClick}
+                onClick={() => {
+                  showUpdate(todo);
+                }}
                 color="secondary"
                 aria-label="edit task"
               >
@@ -225,7 +96,9 @@ export default function Todo({ todo }) {
               <IconButton
                 color="secondary"
                 aria-label="delete task"
-                onClick={handleDeleteClick}
+                onClick={() => {
+                  showDelete(todo);
+                }}
               >
                 <DeleteIcon
                   sx={{
--- src/Components/TodoList/TodoList.jsx
+++ src/Components/TodoList/TodoList.jsx
@@ -18,6 +18,13 @@ import { useState, useContext, useEffect, useMemo } from "react";
 import { v4 as uuidv4 } from "uuid";
 import TodosContext from "../../context/TodosContext";
 
+// import dialogs
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
 /////////////////////
 export default function TodoList() {
   const { todos, setTodos } = useContext(TodosContext);
@@ -27,11 +34,22 @@ export default function TodoList() {
     details: "",
   });
 
+  // state for todo coming from child component in order to using id to update and delete
+  const [dialogTodo, setDialogTodo] = useState(null);
+
+  const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+
+  // state for dialog state change
+  const [showUpdateDialog, setShowUpdateDialog] = useState(false);
+  const [updateTodo, setUpdateTodo] = useState({
+    title: dialogTodo ? dialogTodo.title : "",
+    details: dialogTodo ? dialogTodo.details : "",
+  });
+
   // filtration arrays
   // and using useMemo from performance
   const completedTodos = useMemo(() => {
     return todos.filter((todo) => {
-      console.log("calling complete");
       return todo.isCompleted;
     });
   }, [todos]);
@@ -39,7 +57,6 @@ export default function TodoList() {
   // and using useMemo from performance
   const notCompletedTodos = useMemo(() => {
     return todos.filter((todo) => {
-      console.log("nooooo");
       return !todo.isCompleted;
     });
   }, [todos]);
@@ -51,11 +68,6 @@ export default function TodoList() {
   } else if (displayTodosType == "non-completed") {
     todosToBeRendered = notCompletedTodos;
   }
-
-  // the Todos viewer
-  const todosJsx = todosToBeRendered.map((todo) => {
-    return <Todo key={todo.id} todo={todo} />;
-  });
 
   // call useEffect in order to git data from local storage
   useEffect(() => {
@@ -93,80 +105,207 @@ export default function TodoList() {
   function changeDisplayType(e) {
     setDisplayTodosType(e.target.value);
   }
+
+  // handlers dialog delete
+
+  // delete function and close the dialog
+  function handleDeleteClick(todo) {
+    setOpenDeleteDialog(true);
+    setDialogTodo(todo);
+  }
+
+  function handleDeleteDialogClose() {
+    setOpenDeleteDialog(false);
+  }
+
+  function handleDeleteConfirm() {
+    const newUpdateTodos = todos.filter((t) => t.id !== dialogTodo.id);
+    setTodos(newUpdateTodos);
+    localStorage.setItem("todos", JSON.stringify(newUpdateTodos));
+    handleDeleteDialogClose(false);
+  }
+
+  // update function and close the dialog
+  function handleUpdateClick(todo) {
+    setShowUpdateDialog(true);
+    setDialogTodo(todo);
+  }
+
+  function handleUpdateClose() {
+    setShowUpdateDialog(false);
+  }
+
+  function handleUpdateConfirm() {
+    const newUpdateTodos = todos.map((t) => {
+      return dialogTodo.id === t.id
+        ? { ...t, title: updateTodo.title, details: updateTodo.details }
+        : t;
+    });
+    setTodos(newUpdateTodos);
+    setShowUpdateDialog(false);
+    localStorage.setItem("todos", JSON.stringify(newUpdateTodos));
+  }
+
+  // the Todos jsx viewer
+  const todosJsx = todosToBeRendered.map((todo) => {
+    return (
+      <Todo
+        key={todo.id}
+        todo={todo}
+        showDelete={handleDeleteClick}
+        showUpdate={handleUpdateClick}
+      />
+    );
+  });
   // component todo
   return (
-    <Container maxWidth="sm">
-      <Card style={{ maxHeight: "80vh", overflow: "scroll" }}>
-        <CardContent>
-          <Typography variant="h5">My Tasks</Typography>
-          <Divider sx={{ marginBottom: "7px" }} />
-          <ToggleButtonGroup
-            value={displayTodosType}
-            exclusive
-            onChange={changeDisplayType}
-            color="primary"
-          >
-            <ToggleButton value="all">All</ToggleButton>
-            <ToggleButton value="completed">Success</ToggleButton>
-            <ToggleButton value="non-completed">Not Completed</ToggleButton>
-          </ToggleButtonGroup>
-          {/* components todo */}
-          {todosJsx}
+    <>
+      {/* DELETE DIALOG */}
+      <Dialog
+        open={openDeleteDialog}
+        onClose={handleDeleteDialogClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Are you sure you want to delete this task?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            You cannot undo the deletion after it is complete.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDeleteDialogClose}>Disagree</Button>
+          <Button onClick={handleDeleteConfirm} autoFocus>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+      {/*=== DELETE DIALOG ===*/}
 
-          {/*=== components todo ===*/}
-
-          {/* input add todo */}
-          <Grid
-            container
-            spacing={2}
-            sx={{
-              marginTop: "5px",
-              alignItems: "center",
-              justifyContent: "space-between",
+      {/* UPDATE DIALOG */}
+      <Dialog
+        open={showUpdateDialog}
+        onClose={handleUpdateClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Edit the assignment "}
+        </DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="name"
+            name="email"
+            label="title task name"
+            fullWidth
+            variant="standard"
+            value={updateTodo.title}
+            onChange={(e) => {
+              setUpdateTodo({ ...updateTodo, title: e.target.value });
             }}
-          >
-            <Grid item xs={9}>
-              <TextField
-                id="outlined-basic"
-                label="Task title"
-                variant="outlined"
-                sx={{ width: "100%", marginBottom: "10px" }}
-                value={titleInputAndDetails.title}
-                onChange={(e) => {
-                  setTitleAndDetailsInput({
-                    ...titleInputAndDetails,
-                    title: e.target.value,
-                  });
-                }}
-              />
-              <TextField
-                id="outlined-basic"
-                label="Task details"
-                variant="outlined"
-                sx={{ width: "100%" }}
-                value={titleInputAndDetails.details}
-                onChange={(e) => {
-                  setTitleAndDetailsInput({
-                    ...titleInputAndDetails,
-                    details: e.target.value,
-                  });
-                }}
-              />
+          />
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="name"
+            name="email"
+            label="details"
+            fullWidth
+            variant="standard"
+            value={updateTodo.details}
+            onChange={(e) => {
+              setUpdateTodo({ ...updateTodo, details: e.target.value });
+            }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleUpdateClose}>Disagree</Button>
+          <Button onClick={handleUpdateConfirm} autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+      {/*=== UPDATE DIALOG ===*/}
+
+      <Container maxWidth="sm">
+        <Card style={{ maxHeight: "80vh", overflow: "scroll" }}>
+          <CardContent>
+            <Typography variant="h5">My Tasks</Typography>
+            <Divider sx={{ marginBottom: "7px" }} />
+            <ToggleButtonGroup
+              value={displayTodosType}
+              exclusive
+              onChange={changeDisplayType}
+              color="primary"
+            >
+              <ToggleButton value="all">All</ToggleButton>
+              <ToggleButton value="completed">Success</ToggleButton>
+              <ToggleButton value="non-completed">Not Completed</ToggleButton>
+            </ToggleButtonGroup>
+            {/* components todo */}
+            {todosJsx}
+
+            {/*=== components todo ===*/}
+
+            {/* input add todo */}
+            <Grid
+              container
+              spacing={2}
+              sx={{
+                marginTop: "5px",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Grid item xs={9}>
+                <TextField
+                  id="outlined-basic"
+                  label="Task title"
+                  variant="outlined"
+                  sx={{ width: "100%", marginBottom: "10px" }}
+                  value={titleInputAndDetails.title}
+                  onChange={(e) => {
+                    setTitleAndDetailsInput({
+                      ...titleInputAndDetails,
+                      title: e.target.value,
+                    });
+                  }}
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Task details"
+                  variant="outlined"
+                  sx={{ width: "100%" }}
+                  value={titleInputAndDetails.details}
+                  onChange={(e) => {
+                    setTitleAndDetailsInput({
+                      ...titleInputAndDetails,
+                      details: e.target.value,
+                    });
+                  }}
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <Button
+                  variant="contained"
+                  sx={{ background: "primary" }}
+                  onClick={handelAddClick}
+                  disabled={titleInputAndDetails.title.length == 0}
+                >
+                  Add a task
+                </Button>
+              </Grid>
             </Grid>
-            <Grid item xs={3}>
-              <Button
-                variant="contained"
-                sx={{ background: "primary" }}
-                onClick={handelAddClick}
-                disabled={titleInputAndDetails.title.length == 0}
-              >
-                Add a task
-              </Button>
-            </Grid>
-          </Grid>
-          {/*=== input add todo ===*/}
-        </CardContent>
-      </Card>
-    </Container>
+            {/*=== input add todo ===*/}
+          </CardContent>
+        </Card>
+      </Container>
+    </>
   );
 }
